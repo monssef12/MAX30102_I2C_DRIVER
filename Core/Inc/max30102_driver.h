a7141620a592bacae24e2e391fceaaf6e7e03275
monssef <moncifbakkalli@gmail.com>
2025-07-16 18:01:12 +0100

    first commit

---
+++ Core/Inc/max30102_driver.h
@@ -0,0 +1,46 @@
+/*
+ * max30102_driver.h
+ *
+ *  Created on: Jul 16, 2025
+ *      Author: monci
+ */
+
+#ifndef INC_MAX30102_DRIVER_H_
+#define INC_MAX30102_DRIVER_H_
+
+
+/*
+ * The sensor set of register + the device I2C adress
+ * */
+
+#define MAX30102_ADDR   0b01010111  /*The MAX30102 slave adresse*/
+
+/*STATUS REGISTERS*/
+#define INTERRUPT_STATUS_1    0x00  /*The Interrupt Status 1 register address*/
+#define INTERRUPT_STATUS_2    0x01  /*The Interrupt Status 2 register address*/
+#define INTERRUPT_ENABLE_1    0x02  /*The Interrupt Enable 1 register address*/
+#define INTERRUPT_ENABLE_2    0x03  /*The Interrupt Enable 2 register address*/
+
+#define FIFO_WRITE_PTR        0x04    /*The FIFO Write Pointer register address*/
+#define OVERFLOW_COUNTER      0x05    /*The Overflow Counter register address*/
+#define FIFO_READ_PTR         0x06    /*The FIFO Read Pointer register address*/
+#define FIFO_DATA             0x07    /*The  FIFO Data register address*/
+
+/*CONFIGURATION REGISTERS*/
+#define FIFO_CONFIGURATION        0X08    /*The FIFO CONFIGURATION register address*/
+#define MODE_CONFIGURATION        0X09    /*The MODE CONFIGURATION register address*/
+#define SPO2_CONFIGURATION        0X0A    /*The SpO2 Configuration register address*/
+#define LED_PULSE_AMPLITUDE       0X0C    /*The LED Pulse Amplitude register address*/
+#define LED_PULSE_AMPLITUDE       0X0D    /*The LED Pulse Amplitude register address*/
+#define MULTI_LED_MODE_CONTROL    0X11    /*The Multi-LED Mode Control register address*/
+#define MULTI_LED_MODE_CONTROL    0X11    /*The Multi-LED Mode Control register address*/
+
+/*DIE TEMPERATURE*/
+#define DIE_TEMPERATURE_INTEGER    0X1F    /*The DIE TEMPERATURE INTEGER register address*/
+#define DIE_TEMPERATURE_FRACTION   0X20    /*The DIE TEMPERATURE FRACTION register address*/
+#define DIE_TEMPERATURE_CONFIG   0X20    /*The DIE TEMPERATURE CONFIG register address*/
+
+
+
+
+#endif /* INC_MAX30102_DRIVER_H_ */
---
+++ Core/Src/max30102_driver.c
@@ -0,0 +1,8 @@
+/*
+ * max30102_driver.c
+ *
+ *  Created on: Jul 16, 2025
+ *      Author: monci
+ */
+
+
